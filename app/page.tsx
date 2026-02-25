0e2c381963b76b7a6bfde3026d29a50f2135c35f
Sumit_Rauthan <sumitrauthan413@gmail.com>
2026-02-26 00:26:05 +0530

    add comment in landing page

--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,10 @@ export default function Home() {
       {/* Global Glow Effects */}
       <div className="absolute -top-40 -left-40 w-[500px] h-[500px] bg-yellow-500 blur-[160px] opacity-10 rounded-full" />
       <div className="absolute -bottom-40 -right-40 w-[500px] h-[500px] bg-orange-500 blur-[160px] opacity-10 rounded-full" />
-
+      {/*hero section */}
       <Hero />
+      {/*featured products section */}
       <FeaturedProducts />
-
-     
-      
 
     </main>
   )
